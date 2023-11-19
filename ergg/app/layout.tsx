9904613ab2a8040d172baa88af81ae48309e935d
Kang Ji Seok <36768877+PLNUS@users.noreply.github.com>
2023-11-19 14:58:17 +0000

    set EB CLI

--- ergg/app/layout.tsx
+++ ergg/app/layout.tsx
@@ -1,13 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import Link from 'next/link'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ER Analysis',
-  description: 'Daydreaming in ROKA',
+  title: 'Shoi.ch | 쇼맘이 시켜서 만든 통계사이트 쇼이.치',
+  description: '이터널리턴 통계 제공 웹서비스\n매일 21시쯤 업데이트',
 }
 
 export default function RootLayout({
